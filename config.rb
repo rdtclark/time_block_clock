9c72843cbadb95d68e29beb66e4d169b95767cc6
Robin Clark <robindtclark@gmail.com>
2021-04-12 18:21:19 +0100

    only livereload on development mode

--- config.rb
+++ config.rb
@@ -1,6 +1,12 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+configure :development do
+  activate :livereload
+end
+
+activate :directory_indexes
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
