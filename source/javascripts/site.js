ab31164ae673b20491faf59562e76aea0aa5201a
Robin Clark <robindtclark@gmail.com>
2021-04-12 21:58:37 +0100

    display updated time on the homepage

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+window.onload = function() {
+
+	function refreshTime() {
+		let currentTime = new Date();
+		document.getElementById("current_date").innerHTML = currentTime.toTimeString();
+	};
+
+	setInterval(refreshTime, 1000);
+};
